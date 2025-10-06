b89b4642f28db07192a2494eac9e74e9f0679061
Ali Anwar <dev.alianwar@gmail.com>
2025-10-06 20:56:19 +0500

    Leetcode 622 circular queue solution initalized with ListNode

---
+++ problems/python/leetcode622.py
@@ -0,0 +1,56 @@
+"""
+Leetcode - 622 (AirBnB Interview)
+Design Circular Queue
+
+"""
+
+class ListNode:
+     def __init__(self, val:int, next:int, prev:int):
+        self.val, self.next, self.prev = val, next, prev
+
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        self.space = k
+        self.left = ListNode(0, None, None)
+        self.right = ListNode(0, None, self.right)
+        self.left.next = self.right
+
+    def enQueue(self, value: int) -> bool:
+        
+
+    def deQueue(self) -> bool:
+        
+
+    def Front(self) -> int:
+        
+
+    def Rear(self) -> int:
+        
+
+    def isEmpty(self) -> bool:
+        
+
+    def isFull(self) -> bool:
+        
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
+
+
+
+
+
+
+
+
+
+
+
